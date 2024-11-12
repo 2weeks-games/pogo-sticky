5eb1f1134bf7eaac0f485ae82e80ce21b8a11b2e
nat2weeks <nat@2weeks.games>
2024-11-11 17:21:39 -0800

    Use WASD, HJKL or arrow keys for movement

--- client/input_map.lua
+++ client/input_map.lua
@@ -1,13 +1,13 @@
 local input_map = {}
 
 function input_map.map_keyboard_key(key_name)
-    if key_name == 'H' then
+    if key_name == 'Left' or key_name == 'A' or key_name == 'H' then
         return 'left'
-    elseif key_name == 'L' then
+    elseif key_name == 'Right' or key_name == 'D' or key_name == 'L' then
         return 'right'
-    elseif key_name == 'J' then
+    elseif key_name == 'Down' or key_name == 'S' or key_name == 'J' then
         return 'down'
-    elseif key_name == 'K' then
+    elseif key_name == 'Up' or key_name == 'W' or key_name == 'K' then
         return 'up'
     end
 end
